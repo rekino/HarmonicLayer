8ea548a09d056540c564a716c7c25c45e9b98719
Ramin Barati <ramin.barati@carnext.com>
2023-06-05 15:11:42 +0330

    add layer implementation

---
+++ layer/harmonic.py
@@ -0,0 +1,51 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from itertools import combinations, combinations_with_replacement
+
+from integrate import hermite_gauss, hermite_gauss_2d
+
+
+class HarmonicLayer(nn.Module):
+    def __init__(self, in_features, out_features, biased=True) -> None:
+        super().__init__()
+
+        self.n, self.m = in_features, out_features
+
+        self.w = nn.Parameter(torch.randn(self.n, self.m))
+        self.b = 0
+        if biased:
+            self.b = nn.Parameter(torch.randn(self.m))
+
+        self.act = torch.tanh
+        self.actprime = lambda x: 1 - self.act(x)**2
+
+    def _qr_decompose(self):
+        W = torch.stack([self.w[:, (i, j)] for i, j in combinations(range(self.m), r=2)])
+        _, R_triu = torch.linalg.qr(W)
+        _, R_diag = torch.linalg.qr(self.w.T[:, :, None])
+
+        return R_triu, torch.squeeze(R_diag)
+    
+    def _quad(self, w, b, x):
+        return self.actprime(w*x + b)**2
+    
+    def _cube(self, R_triu, b, x, y):
+        return self.actprime(R_triu[0, 0]*x + b[0]) * self.actprime(R_triu[0, 1]*x + R_triu[1, 1]*y + b[1])
+    
+    def compute_tuning(self, deg):
+        sigma = torch.zeros(self.m, self.m)
+        R_triu, R_diag = self._qr_decompose()
+
+        for i, j in combinations_with_replacement(range(self.m), r=2):
+            if i == j:
+                sigma[i, i] = np.sqrt(np.pi) * hermite_gauss(lambda x: self._quad(R_diag[i], self.b[i], x), deg)
+            else:
+                sigma[i, j] = sigma[j, i] = hermite_gauss_2d(lambda x, y: self._cube(R_triu[i], self.b[[i, j]], x, y), deg)
+        
+        return (self.w.T @ self.w) * sigma
+
+
+    def forward(self, x):
+        return self.act(x @ self.w + self.b)
---
+++ test/test_layer.py
@@ -0,0 +1,76 @@
+import unittest as ut
+import torch
+from scipy.integrate import dblquad, quad
+import numpy as np
+
+from layer import HarmonicLayer
+
+
+class TestLayer(ut.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_qr(self):
+        layer = HarmonicLayer(2, 2)
+        R_triu, R_diag = layer._qr_decompose()
+        R_triu = R_triu.detach().numpy()
+        R_diag = R_diag.detach().numpy()
+        self.assertEqual(len(R_triu.shape), 3)
+        self.assertEqual(R_triu.shape[0], 1)
+        self.assertEqual(len(R_diag.shape), 1)
+        self.assertEqual(R_diag.shape[0], 2)
+
+        w, b = layer.w.detach().numpy(), layer.b.detach().numpy()
+
+        def integrand_00(x, y):
+            return np.tanh(x * w[0, 0] + y * w[1, 0] + b[0])**2 * np.exp(-(x**2 + y**2))
+        
+        s00, err = dblquad(integrand_00, -np.inf, np.inf, -np.inf, np.inf)
+
+        def integrand_00_rotated(x):
+            return np.tanh(x * R_diag[0] + b[0])**2 * np.exp(-x**2)
+        
+        s00_rotated, err = quad(integrand_00_rotated, -np.inf, np.inf)
+
+        self.assertAlmostEqual(s00, np.sqrt(np.pi) * s00_rotated, 5)
+
+        def integrand_01(x, y):
+            return np.tanh(x * w[0, 0] + y * w[1, 0] + b[0]) * np.tanh(x * w[0, 1] + y * w[1, 1] + b[1]) * np.exp(-(x**2 + y**2))
+        
+        s01, err = dblquad(integrand_01, -np.inf, np.inf, -np.inf, np.inf)
+
+        def integrand_01_rotated(x, y):
+            return np.tanh(x * R_triu[0, 0, 0] + b[0]) * np.tanh(x * R_triu[0, 0, 1] + y * R_triu[0, 1, 1] + b[1]) * np.exp(-(x**2 + y**2))
+        
+        s01_rotated, err = dblquad(integrand_01_rotated, -np.inf, np.inf, -np.inf, np.inf)
+
+        self.assertAlmostEqual(s01, s01_rotated, 5)
+
+    
+    def test_tuning(self):
+        torch.manual_seed(1)
+        layer = HarmonicLayer(2, 2)
+        sigma = layer.compute_tuning(20).detach().numpy()
+
+        w, b = layer.w.detach().numpy(), layer.b.detach().numpy()
+
+        def integrand_00(x, y):
+            return (1 - np.tanh(x * w[0, 0] + y * w[1, 0] + b[0])**2)**2 * np.exp(-(x**2 + y**2))
+
+        s00, err = dblquad(integrand_00, -np.inf, np.inf, -np.inf, np.inf)
+        self.assertAlmostEqual(s00 * (w[0, 0]**2 + w[1, 0]**2), sigma[0, 0], 3)
+
+        def integrand_01(x, y):
+            return (1 - np.tanh(x * w[0, 0] + y * w[1, 0] + b[0])**2) * (1 - np.tanh(x * w[0, 1] + y * w[1, 1] + b[1])**2) * np.exp(-(x**2 + y**2))
+
+        s01, err = dblquad(integrand_01, -np.inf, np.inf, -np.inf, np.inf)
+        self.assertAlmostEqual(s01 * (w[0, 0]*w[0, 1] + w[1, 0]*w[1, 1]), sigma[0, 1], 3)
+
+
+    def test_forward(self):
+        layer = HarmonicLayer(2, 2)
+        out = layer(torch.randn(3, 2)).detach().numpy()
+
+        self.assertEqual(len(out.shape), 2)
+        self.assertEqual(out.shape[0], 3)
+        self.assertEqual(out.shape[1], 2)
